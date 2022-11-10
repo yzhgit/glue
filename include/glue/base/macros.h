74367fc82c6b44881b7d300bdc54052d49c89ce7
youlong <youlong@orbbec.com>
2022-11-10 13:54:32 +0800

    -

--- include/glue/base/macros.h
+++ include/glue/base/macros.h
@@ -45,77 +45,21 @@ private:                                                                        
     static void* operator new(size_t) = delete;                                                    \
     static void operator delete(void*) = delete;
 
-//==============================================================================
-// Debugging and assertion macros
-
-#ifndef GLUE_LOG_CURRENT_ASSERTION
-    #if GLUE_LOG_ASSERTIONS || GLUE_DEBUG
-        #define GLUE_LOG_CURRENT_ASSERTION glue::logAssertion(__FILE__, __LINE__);
-    #else
-        #define GLUE_LOG_CURRENT_ASSERTION
-    #endif
-#endif
-
-//==============================================================================
-/** This will try to break into the debugger if the app is currently being debugged.
-    If called by an app that's not being debugged, the behaviour isn't defined - it may
-    crash or not, depending on the platform.
-    @see GLUE_ASSERT()
-*/
-#if defined(GLUE_OS_LINUX) || defined(GLUE_OS_BSD)
-    #define GLUE_BREAK_IN_DEBUGGER ::kill(0, SIGTRAP);
-#elif defined(GLUE_COMPILER_MSVC)
-    #ifndef __INTEL_COMPILER
-        #pragma intrinsic(__debugbreak)
-    #endif
-    #define GLUE_BREAK_IN_DEBUGGER __debugbreak();
-#elif GLUE_ARCH_X86 && defined(GLUE_USE_GCC_INLINE_ASM)
-    #define GLUE_BREAK_IN_DEBUGGER asm("int $3");
-#elif defined(GLUE_OS_ANDROID)
-    #define GLUE_BREAK_IN_DEBUGGER __builtin_trap();
+// GLUE_ASSERT()
+//
+// In C++11, `assert` can't be used portably within constexpr functions.
+// GLUE_ASSERT functions as a runtime assert but works in C++11 constexpr
+// functions.  Example:
+//
+// constexpr double Divide(double a, double b) {
+//   return GLUE_ASSERT(b != 0), a / b;
+// }
+//
+// This macro is inspired by
+// https://akrzemi1.wordpress.com/2017/05/18/asserts-in-constexpr-functions/
+#if defined(NDEBUG)
+    #define GLUE_ASSERT(expr) (false ? static_cast<void>(expr) : static_cast<void>(0))
 #else
-    #define GLUE_BREAK_IN_DEBUGGER __asm int 3
+    #define GLUE_ASSERT(expr)                                                                      \
+        (GLUE_LIKELY((expr)) ? static_cast<void>(0) : [] { assert(false && #expr); }())
 #endif
-
-//==============================================================================
-#if GLUE_MSVC && !defined(DOXYGEN)
-    #define GLUE_WHILE_LOOP(x)                                                                     \
-        __pragma(warning(push)) __pragma(warning(disable : 4127)) do                               \
-        {                                                                                          \
-            x                                                                                      \
-        }                                                                                          \
-        while (false) __pragma(warning(pop))
-#else
-    /** This is the good old C++ trick for creating a macro that forces the user to put
-       a semicolon after it when they use it.
-    */
-    #define GLUE_WHILE_LOOP(x)                                                                     \
-        do {                                                                                       \
-            x                                                                                      \
-        } while (false)
-#endif
-
-//==============================================================================
-/** This will always cause an assertion failure.
-    It is only compiled in a debug build, (unless GLUE_LOG_ASSERTIONS is enabled for your
-   build).
-    @see jassert
-*/
-#define jassertfalse GLUE_WHILE_LOOP(GLUE_LOG_CURRENT_ASSERTION)
-
-/** Platform-independent assertion macro.
-
-    This macro gets turned into a no-op when you're building with debugging turned off, so be
-    careful that the expression you pass to it doesn't perform any actions that are vital for
-   the correct behaviour of your program!
-    @see jassertfalse
-*/
-#define jassert(expression) GLUE_WHILE_LOOP(if (!(expression)) jassertfalse;)
-
-/** Platform-independent assertion macro which suppresses ignored-variable
-    warnings in all build modes. You should probably use a plain jassert()
-    by default, and only replace it with jassertquiet() once you've
-    convinced yourself that any unused-variable warnings emitted by the
-    compiler are harmless.
-*/
-#define jassertquiet(expression) GLUE_WHILE_LOOP(if (!(expression)) jassertfalse;)
